0a48c19e31189f5cd88462bb01b45a130f7a46ea
elindi <prostonaf@gmail.com>
2025-04-17 02:48:55 +1000

    hardcoded styles just to be

--- src/pages/Main-Page/Main-Page.tsx
+++ src/pages/Main-Page/Main-Page.tsx
@@ -1,6 +1,6 @@
 export const MainPage = () => {
 	return (
-		<div>
+		<div style={{alignItems: 'center', display: 'flex', height: '100vh', justifyContent: 'center'}}>
 			<h2>This is the main page</h2>
 		</div>
 	)
--- src/pages/Test-Page/Test-Page.tsx
+++ src/pages/Test-Page/Test-Page.tsx
@@ -1,7 +1,7 @@
 export const TestPage = () => {
 	return (
-		<div>
-			<h2>This is a test route huy</h2>
+		<div style={{alignItems: 'center', display: 'flex', height: '100vh', justifyContent: 'center'}}>
+			<h2>This is a test route</h2>
 		</div>
 	)
 }
